b5b7888262bc8abf813631dc87cd1aef34729170
gravis3000 <134720300+gravis3000@users.noreply.github.com>
2023-05-28 02:25:23 +0000

    Initial commit

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -8,12 +8,9 @@ export type ExampleModel = {
 };
 
 const EXAMPLES: ExampleModel[] = [
-    {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
-    },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What was HKEX's revenue in 2022?", value: "What was HKEX's revenue in 2022?" },
+    { text: "What was HKEX's EBITDA in 2021?", value: "What was HKEX's EBITDA in 2021?"},
+    { text: "When did HKEX announce its 2022 final results?", value: "When did HKEX announce its 2022 final results?"}
 ];
 
 interface Props {
